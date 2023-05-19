ac02271ec223656193b21ecf4e168f7a8851d401
AndraAhmadFadhil <andraahmadfadil@gmail.com>
2023-05-19 16:27:17 +0700

    membuat case 2

--- linkedlistoop/linkedlistoop.cpp
+++ linkedlistoop/linkedlistoop.cpp
@@ -144,4 +144,18 @@ int main()
 		}
 		break;
 
+		case '2':
+		{
+			if (mhs.listEmpty())
+			{
+				cout << endl << "List Kosong" << endl;
+				break;
+			}
+			cout << endl << "\nMasukkan no mahasiswa yang akan dihapus : ";
+			cin >> nim;
+			if (mhs.delNote(nim) == false)
+				cout << endl << "Data tidak ditemukan" << endl;
+			else
+				cout << endl << "Data dengan nomor mahasiswa " << nim << "berhasil dihapus " << endl;
+		}
 		
