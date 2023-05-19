95a1e94690d60ca00fd2008c64893a70499f07da
AndraAhmadFadhil <andraahmadfadil@gmail.com>
2023-05-19 15:18:55 +0700

    menambah sebuah Node kedalam list

--- linkedlistoop/linkedlistoop.cpp
+++ linkedlistoop/linkedlistoop.cpp
@@ -11,3 +11,22 @@ public:
 };
 
 class list
+{
+	Node* START;
+public:
+	list();
+	void addNote();
+	bool Search(int nim, Node** current, Node** previous);
+	bool listEmpty();
+	bool delNote(int element);
+	void traverse();
+};
+
+list::list()
+{
+	START = NULL;
+}
+void list::addNote() 
+{
+
+}
